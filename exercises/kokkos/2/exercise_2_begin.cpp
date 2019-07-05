6adb7ce1b699bcbc268857793b0dc2d80a00cfa7
preich <reichp@gmail.com>
2019-07-05 15:20:33 +0100

    added block init/finalise

--- exercises/kokkos/2/exercise_2_begin.cpp
+++ exercises/kokkos/2/exercise_2_begin.cpp
@@ -97,7 +97,7 @@ int main( int argc, char* argv[] )
   checkSizes( N, M, S, nrepeat );
 
   Kokkos::initialize( argc, argv );
-
+  {
   // EXERCISE: Create views of the right size.
 
   // 1. Device Views
@@ -186,7 +186,7 @@ int main( int argc, char* argv[] )
   //delete  y;  //EXERCISE hint: ...
   //delete  x;  //EXERCISE hint: ...
   //delete  A;  //EXERCISE hint: ...
-
+  }
   Kokkos::finalize();
 
   return 0;
